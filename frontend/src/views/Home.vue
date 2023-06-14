5ef6a99eecdd78ecc5ad28a6e56472724fc82bc1
Hugo BRETECHE <hubretec@paul-f3Br1s1.clusters.42paris.fr>
2023-06-14 15:10:42 +0200

    Need to move channels in the database

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,30 +3,40 @@
 	<div class="home_body">
 		<div class="home_content">
 			<div class="left_column">
-				<router-link class="play_button" to="/pong">jouer</router-link>	
+				<router-link class="play_button" to="/pong">jouer</router-link>
 				<div class="match_historic">
 					<label>macht_historic</label>
 				</div>
 			</div>
 			<div class="chat">
-				Chat
-				<ul class="msg_chat_box">
-					<li v-for="msg in messages" :key="msg.id" :class="getMessageClass(msg)">{{ msg.text }}</li>
-				</ul>
-				<div v-if="connected">
-					<input type="text" v-model="message">
-					<button type="button" @click="sendMessage">Send !</button>
-				</div>
-				<div v-else>
-					<p>connecting to websocket server...</p>
+				<div v-if="showDiv">
+					Chat
+					<h2>{{ selectedChannel.name }}</h2>
+					<ul class="msg_chat_box">
+						<li v-for="msg in selectedChannel.messages" :key="msg.id" :class="getMessageClass(msg)">{{ msg.text }}</li>
+					</ul>
+					<div v-if="connected">
+						<input type="text" v-model="message">
+						<button type="button" @click="sendMessage">Send !</button>
+					</div>
+					<div v-else>
+						<p>connecting to websocket server...</p>
+					</div>
 				</div>
 			</div>
 			<div class="right_column">
 				<div class="friend_list">
 					<label>friend_list</label>
+					<ul>
+						<li v-for="channel in channels"><button :key="channel.id" @click="showChannel(channel.id)">{{ channel.name }}</button></li>
+					</ul>
 				</div>
 				<div class="join_panel">
 					<label>join_panel</label>
+					<label for="channel_field">Join a channel or add a friend</label>
+					<input type="text" name="channel_field" v-model="channelName">
+					<input type="button" value="Create/Join channel" @click="createChannel(channelName)">
+					<input type="button" value="Add a friend" @click="addFriend(channelName)">
 				</div>
 			</div>
 		</div>
@@ -34,6 +44,11 @@
 </template>
 
 <script lang="ts">
+
+/*
+ 1. Will have to push the channels (and friends) in the database
+*/
+
 import Head from '../components/head.vue'
 import io from 'socket.io-client';
 import { defineComponent } from 'vue';
@@ -44,6 +59,20 @@ interface Message {
 	sender: number,
 };
 
+export class Channel {
+	public id: number;
+	public name: string;
+	public messages: Message[];
+	public users: number[];
+
+	constructor(name: string, id: number) {
+		this.id = id;
+		this.messages = [];
+		this.users = [];
+		this.name = name;
+	}
+};
+
 export default defineComponent({
 	data() {
 		return {
@@ -52,14 +81,17 @@ export default defineComponent({
 			message: '' as string,
 			messages: [] as Message[],
 			sender: -1 as number,
+			channels: [] as Channel[],
+			selectedChannel: {} as Channel,
+			showDiv: false as Boolean,
 		};
 	},
 
 	async mounted() {
-		const response = await fetch("http://" + import.meta.env.VITE_HOST + ":3000/user/me", {credentials: 'include'});
+		const response = await fetch("http://" + import.meta.env.VITE_HOST + ":3000/user/me", { credentials: 'include' });
 		const response_json = await response.json();
 		this.sender = response_json['id'];
-		this.initSocket();		
+		this.initSocket();
 	},
 
 	methods: {
@@ -67,30 +99,58 @@ export default defineComponent({
 			this.socket = io('http://localhost:3000/');
 			this.socket.on('connect', () => { this.connected = true; });
 			this.socket.on('disconnect', () => { this.connected = false; });
-			this.socket.on('message', (data: {text: string, sender: number}) => {
-				const {text, sender} = data;
+			this.socket.on('message', (data: { text: string, sender: number }) => {
+				const { text, sender } = data;
 				if (sender !== this.sender) {
-					this.messages.push({
-						id: this.messages.length + 1,
+					this.selectedChannel.messages.push({
+						id: this.selectedChannel.messages.length + 1,
 						text: text,
 						sender: sender,
 					});
 				}
 			});
 		},
+
 		sendMessage() {
 			if (this.message && this.socket) {
-				this.socket.emit('message', {text: this.message, sender: this.sender});
-				this.messages.push({
-					id: this.messages.length + 1,
+				this.socket.emit('message', { text: this.message, sender: this.sender });
+				this.selectedChannel.messages.push({
+					id: this.selectedChannel.messages.length + 1,
 					text: this.message,
 					sender: this.sender,
 				});
 				this.message = '';
 			}
 		},
+
 		getMessageClass(message: Message): string {
 			return (this.sender === message.sender ? 'sent' : 'received');
+		},
+
+		// Maybe collapse these two functions into one
+		createChannel(name: string): void {
+			name = '#' + name;
+			for (let i = 0; i < this.channels.length; i++) {
+				if (this.channels[i].name === name) {
+					if (!(this.channels[i].users.includes(this.sender))) {
+						this.channels[i].users.push(this.sender);
+					}
+					else {
+						this.selectedChannel = this.channels[i];
+					}
+					return;
+				}
+			}
+			this.channels.push(new Channel(name, this.channels.length));
+		},
+
+		addFriend(name: string): void {
+			console.log('@' + name);
+		},
+
+		showChannel(id: number): void {
+			this.showDiv = true;
+			this.selectedChannel = this.channels[id];
 		}
 	},
 	components: {
@@ -198,5 +258,4 @@ export default defineComponent({
 .received {
 	text-align: left;
 }
-
 </style>
