05663784f620e9b90e0d1823e97273c01c098921
Jules ROSSETTI <jrossett@paul-f3Br4s8.clusters.42paris.fr>
2023-06-16 15:54:51 +0200

    header image profile avancement

--- frontend/src/components/head.vue
+++ frontend/src/components/head.vue
@@ -1,8 +1,20 @@
+<script setup lang="ts">
+
+async function header_data() {
+	const response = await fetch("http://" + import.meta.env.VITE_HOST + ":3000/user/me", {credentials: 'include'});
+	const text = await response.json();
+	console.log(text);
+	const avatar = text["avatarLink"];
+	const name = text["name"];
+}
+
+</script>
+
 <template>
 <header class="header">
 	<router-link to="/" class="logo" ><img class="logo_42" src="../assets/img/logo.png" alt="logo 42"></router-link>
 	<div class="profile_container">
-		<router-link class="box_img_profile" to="/profile"><img class="img_profile" src="../assets/img/default_profile.jpg" alt="default profile img"></router-link>
+		<router-link class="box_img_profile" @click="header_data()" to="/profile"><img class="img_profile" :src="header_data()" alt="default profile img"></router-link>
 		<router-link class="profile" to="/profile">Mon Profil</router-link>
 	</div>
 </header>
@@ -11,14 +23,13 @@
 <style>
 
 .profile_container {
-	width: 25%;
 	display: flex;
 	flex-direction: row;
+	flex: 0.18 1 auto
 }
 
 .header {
 	display: flex;
-	width: 100%;
 	height: 10vh;
 	border-bottom: 2px solid #BC0002;
 	background: #036280;
@@ -28,7 +39,6 @@
 
 header .logo {
 	display: flex;
-	height: 95%;
 	border: 3px solid #BC0002;
 	background: #ffffff;
 	border-radius: 290px;
@@ -45,7 +55,7 @@ header .profile {
 	display: flex;
 	width: 100%;
 	background: #D9D9D9;
-	font-size: 100%;
+	font-size: 1.7em;
 	align-items: center;
 	justify-content: center;
 	text-decoration: none;
@@ -55,15 +65,6 @@ header .profile {
 header .box_img_profile {
 	display: flex;
 	justify-content: end;
-	width: auto;
-	height: 100%;
-
-}
-
-@media screen and (max-width: 750px) {
-	.profile_container {
-			width: 50%
-	}
 }
 
 </style>
